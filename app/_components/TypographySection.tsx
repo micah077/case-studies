c21e1d6654aced8ba3bc34eea929e0a1a256b430
micah <micah@strugbits.com>
2024-11-19 17:19:25 +0500

    fixed typography animation issue

--- app/_components/TypographySection.tsx
+++ app/_components/TypographySection.tsx
@@ -108,11 +108,11 @@ const TypographySection = ({ data }: { data: IData }) => {
                         <div className="flex items-end">
                             <div className={`${isMobile ? "mr-[-20px] mb-[-60px] " : isTablet ? "mr-[-100px]" : "mr-[-120px]"} relative z-10`}>
                                 <Image alt='mobile' height={isMobile ? 60 : isTablet ? 269 : 540} width={isMobile ? 124 : isTablet ? 130 : 261} className='object-contain mr-[-20px] z-10' src={data.typographyLaptop.mobileScreen.src} />
-                                <img className={`absolute ${isMobile ? "top-8 left-[50px]" : isTablet ? "top-6 left-[30px]" : "top-16 left-[-40px]"} ${isMobile && "h-[40.18px] w-[66.81px]"}`} src={data.typographyLaptop.mobileLeft.src} style={leftImageStyle} />
+                                <img className={`absolute ${isMobile ? "top-8 left-[50px]" : isTablet ? "top-6 left-[30px]" : "top-16 left-[0px]"} ${isMobile && "h-[40.18px] w-[66.81px]"}`} src={data.typographyLaptop.mobileLeft.src} style={leftImageStyle} />
                             </div>
                             <div className="relative">
                                 <Image width={isMobile ? 261 : isTablet ? 566 : 1121.37} height={isMobile ? 156 : isTablet ? 338 : 659} alt='laptop' className='object-contain z-1' src={data.typographyLaptop.laptopScreen.src} />
-                                <img className={`absolute  ${isMobile ? "h-[87.49px] w-[66.61px] top-8 right-[50px]" : isTablet ? "h-[187.49px] w-[143.61px] top-10 right-[50px]" : "top-24 right-[100px]"}`} src={data.typographyLaptop.laptopEight.src} style={rightImageStyle} />
+                                <img className={`absolute  ${isMobile ? "h-[87.49px] w-[66.61px] top-8 right-[50px]" : isTablet ? "h-[187.49px] w-[143.61px] top-10 right-[50px]" : "top-24 right-[140px]"}`} src={data.typographyLaptop.laptopEight.src} style={rightImageStyle} />
                             </div>
                         </div>
                     </div>
